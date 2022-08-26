451c1f12d5d0debe159b79b777c0aae28d31fddb
Iain Ballard <iain.ballard@ewater.services>
2022-08-26 09:57:41 +0100

    nothing quite like compiler bugs

---
+++ ProtocolTests/SmallTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using VirtualVpn;
+
+namespace ProtocolTests;
+
+[TestFixture]
+public class SmallTests
+{
+    [Test]
+    public void enum_ordering_works()
+    {
+        Assert.True(LogLevel.Trace < LogLevel.Crypto, "enum order");
+    }
+}
--- VirtualVpn/Crypto/Integrity.cs
+++ VirtualVpn/Crypto/Integrity.cs
@@ -79,7 +79,7 @@ public class Integrity
     public byte[] Compute(byte[] key, byte[] data)
     {
         var full = _algo(key, data);
-        Log.Crypto("------HASH----->\r\n" + Bit.Describe("key", key) + Bit.Describe("data", data) + Bit.Describe("ICV (hash)", full));
+        //Log.Crypto("------HASH----->\r\n" + Bit.Describe("key", key) + Bit.Describe("data", data) + Bit.Describe("ICV (hash)", full));
         if (full.Length <= HashSize) return full;
         return full.Take(HashSize).ToArray();
     }
